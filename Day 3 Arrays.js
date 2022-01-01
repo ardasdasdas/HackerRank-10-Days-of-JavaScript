e79c06f681bb45f125246cf53e94ad5f3209e81d
Muzaffer Arda Uslu <53192718+ardasdasdas@users.noreply.github.com>
2022-01-01 21:19:33 +0300

    Day 4

---
+++ Day 3 Arrays.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let largestNum = nums.indexOf(0);
+    let secondLargest;
+    for(let num of nums){
+        if(largestNum < num){        
+            secondLargest = largestNum;
+            largestNum = num;
+            continue;
+        }
+        if(secondLargest < num && num < largestNum){
+            secondLargest = num;
+        }
+    }
+    return secondLargest;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
