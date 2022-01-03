be6fe1e9b94d9c5cb7632876c86b67dffa230b15
Muzaffer Arda Uslu <53192718+ardasdasdas@users.noreply.github.com>
2022-01-03 21:14:13 +0300

    Day 6

---
+++ Day 5 Arrow Functions.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    let returnArr = [];
+    for(let num of nums){
+        if(num % 2 == 0)
+            returnArr.push(num * 2);
+        else returnArr.push(num * 3);
+    }
+    return returnArr;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
