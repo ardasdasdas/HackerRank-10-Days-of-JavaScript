c8643a76cff5cb79d53d4e6c1348cacdb40c815e
Muzaffer Arda Uslu <53192718+ardasdasdas@users.noreply.github.com>
2022-01-04 21:00:25 +0300

    Day 7

---
+++ Day 6 Bitwise Operators.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getMaxLessThanK(n, k){
+    let maxValue = 0;
+    for(let i = 1; i <= n; i++){
+        for(let j = i + 1; j <= n; j++){
+            if((i & j) > maxValue && (i & j) < k)
+                maxValue = i & j;
+        }
+    }
+    return maxValue;
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
